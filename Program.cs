ac5886cc9cfe7bf17ac7568453c87fde384e26c8
Ric <ruizricardogame@gmail.com>
2026-05-22 11:59:54 -0600

    First develop

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ Una tienda de tecnología desea registrar las ventas realizadas durante una sema
 El sistema será desarrollado en equipos de 4 estudiantes utilizando arreglos en C#.*/
 
 //Integrante 1, Ricardo Hamad
-
-
-
+        string[] nombres = new string[10];
+        double[] precios = new double[10];
+        int[] cantidades = new int[10];
+        int contadorProductos = 0; 
 //Integrante 2, Sergio Cabrera
 
 
