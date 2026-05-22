f1499a746bf8b7a9d433da4534b4a9885511a463
Ric <ruizricardogame@gmail.com>
2026-05-22 11:51:46 -0600

    arreglos

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
 
 Una tienda de tecnología desea registrar las ventas realizadas durante una semana.
 El sistema será desarrollado en equipos de 4 estudiantes utilizando arreglos en C#.*/
-
+using System;
 //Integrante 1, Ricardo Hamad
-
-
-
+        string[] nombres = new string[10];
+        double[] precios = new double[10];
+        int[] cantidades = new int[10];
+        int contadorProductos = 0; 
 //Integrante 2, Sergio Cabrera
 
 
