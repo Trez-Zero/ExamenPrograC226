61e2dac85daf00838d15a1547e805e72238ec9c9
Ric <ruizricardogame@gmail.com>
2026-05-22 11:34:45 -0600

    Se contextualiza el programa

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Contexto
+
+Una tienda de tecnología desea registrar las ventas realizadas durante una semana.
+El sistema será desarrollado en equipos de 4 estudiantes utilizando arreglos en C#.*/
+
+//Integrante 1, Ricardo Hamad
+
+
+
+//Integrante 2, Sergio Cabrera
+
+
+
+//Integrante 3, Jonas Hodson
+
+
+
+//Integrante 4, Ervin Rocha
+
